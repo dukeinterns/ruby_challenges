1b90d6901379e32ea14078a594aab691cfd6824f
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-06-06 12:05:12 -0400

    fizzbuzz

--- if_challenge.rb
+++ if_challenge.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
+
 # Write a method which returns:
 #
 #   * "Fizz" if the number is divisible by 3
@@ -13,10 +14,32 @@ require 'minitest/pride'
 
 # WRITE YOUR CODE HERE.  Name your method `fizzbuzz`.
 
+
+def fizzbuzz(x)
+
+  if x % 3 == 0 && x % 5 == 0
+  "FizzBuzz"
+  elsif x % 3 == 0
+  puts "Fizz"
+  return "Fizz"
+  elsif x % 5 == 0
+   "Buzz"
+  else
+   x
+  end
+
+end
+
+fizzbuzz(25)
+
+
+
 class IfChallenge < MiniTest::Test
   def test_one
-    assert_equal 1, fizzbuzz(1)
+   assert_equal 1, fizzbuzz(1)
   end
+
+
 
   def test_three
     assert_equal "Fizz", fizzbuzz(3)
@@ -34,15 +57,12 @@ class IfChallenge < MiniTest::Test
     assert_equal "Buzz", fizzbuzz(10)
   end
 
-  def test_fifteen
-    assert_equal "FizzBuzz", fizzbuzz(15)
-  end
 
   def test_twenty_three
     assert_equal 23, fizzbuzz(23)
   end
 
-  def test_thirty
+   def test_thirty
     assert_equal "FizzBuzz", fizzbuzz(30)
-  end
+end
 end
