23008aa26f3df3d26ae86ab3d439317b61843c50
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-06-07 11:00:01 -0400

    user input

--- if_challenge.rb
+++ if_challenge.rb
@@ -1,68 +1,102 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-
-
-# Write a method which returns:
+# require 'minitest/autorun'
+# require 'minitest/pride'
 #
-#   * "Fizz" if the number is divisible by 3
-#   * "Buzz" if the number is divisible by 5
-#   * "FizzBuzz" if the number is divisible by 3 and 5
-#   * Otherwise, return the number itself
 #
-# Remember that the % operator (modulo) is your friend.  It returns a zero if one
-# number is divisible by another number.  In other words, 4 % 2 == 0.
+# # Write a method which returns:
+# #
+# #   * "Fizz" if the number is divisible by 3
+# #   * "Buzz" if the number is divisible by 5
+# #   * "FizzBuzz" if the number is divisible by 3 and 5
+# #   * Otherwise, return the number itself
+# #
+# # Remember that the % operator (modulo) is your friend.  It returns a zero if one
+# # number is divisible by another number.  In other words, 4 % 2 == 0.
+#
+# # WRITE YOUR CODE HERE.  Name your method `fizzbuzz`.
+#
+#
+# def fizzbuzz(x)
+#
+#   if x % 3 == 0 && x % 5 == 0
+#   "FizzBuzz"
+#   elsif x % 3 == 0
+#   puts "Fizz"
+#   return "Fizz"
+#   elsif x % 5 == 0
+#    "Buzz"
+#   else
+#    x
+#   end
+#
+# end
+#
+# fizzbuzz(25)
+#
+#
+#
+# class IfChallenge < MiniTest::Test
+#   def test_one
+#    assert_equal 1, fizzbuzz(1)
+#   end
+#
+#
+#
+#   def test_three
+#     assert_equal "Fizz", fizzbuzz(3)
+#   end
+#
+#   def test_five
+#     assert_equal "Buzz", fizzbuzz(5)
+#   end
+#
+#   def test_eight
+#     assert_equal 8, fizzbuzz(8)
+#   end
+#
+#   def test_ten
+#     assert_equal "Buzz", fizzbuzz(10)
+#   end
+#
+#
+#   def test_twenty_three
+#     assert_equal 23, fizzbuzz(23)
+#   end
+#
+#    def test_thirty
+#     assert_equal "FizzBuzz", fizzbuzz(30)
+# end
+# end
 
-# WRITE YOUR CODE HERE.  Name your method `fizzbuzz`.
-
-
-def fizzbuzz(x)
-
-  if x % 3 == 0 && x % 5 == 0
-  "FizzBuzz"
-  elsif x % 3 == 0
-  puts "Fizz"
-  return "Fizz"
-  elsif x % 5 == 0
-   "Buzz"
-  else
-   x
-  end
-
-end
-
-fizzbuzz(25)
 
 
 
-class IfChallenge < MiniTest::Test
-  def test_one
-   assert_equal 1, fizzbuzz(1)
-  end
 
 
 
-  def test_three
-    assert_equal "Fizz", fizzbuzz(3)
-  end
-
-  def test_five
-    assert_equal "Buzz", fizzbuzz(5)
-  end
-
-  def test_eight
-    assert_equal 8, fizzbuzz(8)
-  end
-
-  def test_ten
-    assert_equal "Buzz", fizzbuzz(10)
-  end
 
 
-  def test_twenty_three
-    assert_equal 23, fizzbuzz(23)
-  end
 
-   def test_thirty
-    assert_equal "FizzBuzz", fizzbuzz(30)
-end
-end
+
+
+
+
+
+
+puts "Enter the number you desire: "
+
+x = gets
+puts "the number entered: #{x}"
+sum = x+x
+# x+x/2 = average
+#
+# if user_input == puts "Enter another number: "
+#   x = gets
+# else exit
+# end
+#
+#
+#
+#
+#
+# puts "The total of all the nnumbers you listed is: #{sum}"
+# puts "The Average of all the numbers you listed is:#{average} "
