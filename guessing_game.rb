79f9a28263b0a7816feee7b90fdda845ca717397
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-06-21 10:31:48 -0400

    #3.0

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,13 +1,43 @@
 x = rand(50)
+# attempts == gets.chomp
+  # if attempts >= 7
+  #   break
 loop do
   puts " Guess the number!"
-  if x == gets.chomp.to_i
-  puts "Your guess was correct! (:"
-    break
-  else
+  if x != gets.chomp.to_i
     puts "incorrect guess:("
-    puts "The correct number was : #{x}"
-    break
 
+  elsif
+    
+  elsif x > gets.chomp.to_i
+    puts "The number you entered was too high"
+  else x < get.chomp.to_i
+    puts "The number you entered was too low"
+    puts "The correct number was : #{x}"
 end
 end
+end
+end
+
+
+# x = rand(50)
+# # attempts == gets.chomp
+#   # if attempts >= 7
+#   #   break
+# loop do
+#   puts " Guess the number!"
+#   if x ==gets.chomp.to_i
+#     puts "Your guess was correct! (:"
+#     break
+#   elsif
+#     puts "incorrect guess:("
+#   elsif x > gets.chomp.to_i
+#     puts "The number you entered was too high"
+#   else x < get.chomp.to_i
+#     puts "The number you entered was too low"
+#     puts "The correct number was : #{x}"
+# end
+# end
+# end
+# end
+#
