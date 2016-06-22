9482fb328068dcf5bdc59107ca2994380e01c1f8
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-06-22 10:42:03 -0400

    guessing game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,28 +1,25 @@
 attempts = 7
 randomnum = rand(1...50)
 
-until attempts > 7
+until attempts == 0
   puts "guesses left : #{attempts}"
   print "guess a number from 1-50 : "
   x = gets.chomp.to_i
-  if x == "exit"
-    break
-  elsif x != "0"
-    puts " #{x} is either an iraational number or a string, to fix this enter a vaild number"
-
-  # elsif x == "#{x}"
-  #   puts "you're repreating yourself"
-  elsif x > randomnum
-    puts " The number you entered was too high !"
+  if x > randomnum
+    puts " Your guess was too high !"
   elsif x < randomnum
-    puts "The number you entered was too low!"
-  else x == randomnum
+    puts "Your guess was too low!"
+  elsif x == randomnum
     puts "Your guess was correct!"
+  elsif x != " "
+    puts "error: to fix this enter a vaild number"
+  else x == "exit"
+      break
   end
     attempts -= 1
-
 end
   puts "Game over the answer is : #{randomnum}"
+
 
 # x = rand(50)
 # # attempts == gets.chomp
