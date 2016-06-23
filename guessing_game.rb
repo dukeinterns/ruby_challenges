f23743ab5645a4fb229eccea9030a6cf2d06134d
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-06-23 14:27:50 -0400

    guessing game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,25 +1,31 @@
 attempts = 7
 randomnum = rand(1...50)
-
+puts randomnum
 until attempts == 0
   puts "guesses left : #{attempts}"
   print "guess a number from 1-50 : "
+
   x = gets.chomp.to_i
+
   if x > randomnum
     puts " Your guess was too high !"
   elsif x < randomnum
     puts "Your guess was too low!"
   elsif x == randomnum
     puts "Your guess was correct!"
-  elsif x != " "
-    puts "error: to fix this enter a vaild number"
-  else x == "exit"
       break
+  elsif x == " "
+    puts "invalid input, try again"
+    attempts += 1
+  elsif x == ""
+    puts "no input entered, try again"
+    attempts += 1
+  elsif x == "exit"
+    break
   end
     attempts -= 1
 end
   puts "Game over the answer is : #{randomnum}"
-
 
 # x = rand(50)
 # # attempts == gets.chomp
---
+++ multiply.rb
@@ -0,0 +1,16 @@
+def multiply(45)
+  puts multiply 45*2
+end
+
+
+
+
+
+
+
+
+
+#
+# def multiply (x,y)
+#   return "multiply" + x,y
+# end
