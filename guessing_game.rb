3b05967a55be6c080cdfc5dbd8c2485794ddf372
Danai Adkisson <da129@duke.edu>
2016-06-23 13:11:39 -0400

    add goat challenge

--- guessing_game.rb
+++ guessing_game.rb
@@ -10,8 +10,6 @@ until attempts > 7
   elsif x != "0"
     puts " #{x} is either an iraational number or a string, to fix this enter a vaild number"
 
-  # elsif x == "#{x}"
-  #   puts "you're repreating yourself"
   elsif x > randomnum
     puts " The number you entered was too high !"
   elsif x < randomnum
@@ -23,21 +21,3 @@ until attempts > 7
 
 end
   puts "Game over the answer is : #{randomnum}"
-
-# x = rand(50)
-# # attempts == gets.chomp
-#   # if attempts >= 7
-#   #   break
-# loop do
-#   puts " Guess the number!"
-#   if x ==gets.chomp.to_i
-#     puts "Your guess was correct! (:"
-#     break
-#   elsif
-#     puts "incorrect guess:("
-#   elsif x > gets.chomp.to_i
-#     puts "The number you entered was too high"
-#   else x < get.chomp.to_i
-#     puts "The number you entered was too low"
-#     puts "The correct number was : #{x}"
-# end
