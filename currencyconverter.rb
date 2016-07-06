743529e0184fbd75cdc7264364f0b54b4128081f
Maricarmen Paz Hernandez <maricarmenpazh@gmail.com>
2016-07-06 12:38:08 -0400

    currecy

--- currencyconverter.rb
+++ currencyconverter.rb
@@ -1,12 +1,15 @@
-class Converge
-  attr_reader :usd,:eur,:jap,:mex,:gbp:
-  def initialize({usd: 1.0, eur: 0.90, jap: 102,mex: 18.93,gbp:0.75)
-    @usd = {usd: 1.0, eur: 0.90, jap: 102,mex: 18.93,gbp:0.75}
-    @eur = {eur:1.0, usd:1.11, jap:137.20, mex:20.97, gbp:0.83}
-    @jap = {jap: 1.0, usd:0.0098,eur: 0.0088, mex:0.18 , gbp:0.0073}
-    @mex = {mex: 1.0, usd:0.0053,eur:0.048,jap:5.42, gbp: 0.040}
-    @gbp = {gbp: 1.0: usd: 1.33, eur: 1.21, jap:113.61 , mex: 20.97}
-    .each do
-currency_converter.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD))
-  end
+require "./currencycode.rb"
+
+class CurrencyConverter
+    def initialize(denomination:)
+      euro = { "€" => 0.90 }
+      mex = {"$" => 18.93 }
+      jap = {"¥" => 102 }
+      gbp = {"£" => 0..75 }
+
+end
+  puts" Enter the amount of USD you would like to convert:"
+  x = gets.chomp.to_i
+  puts " you entered $#{x}, what currency would you like to convert to?"
+  y = gets.chomp.to_i
 end
